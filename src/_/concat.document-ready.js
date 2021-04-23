bbf135d7171b7eb5b30d59de7212babbacc6e11a
caezar47 <iss47@ya.ru>
2021-04-23 14:33:31 +0300

    adaptive

--- src/_/concat.document-ready.js
+++ src/_/concat.document-ready.js
@@ -1,1 +1,3 @@
 $("img").addClass("img-responsive");
+
+$(document).width()<768&&$("#img").sticky({topSpacing:0,className:"is--sticky"});
---
+++ src/block/plugin/sticky/.document-ready.js
@@ -0,0 +1,6 @@
+if($(document).width() < 768) {
+	$("#img").sticky({
+		topSpacing:0,
+		className: 'is--sticky'
+	});
+} 

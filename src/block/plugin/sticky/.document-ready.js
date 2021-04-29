8020f7e36b9e0b9a835c80b7079245d03e724c12
caezar47 <iss47@ya.ru>
2021-04-29 14:21:53 +0300

    тени

--- src/block/plugin/sticky/.document-ready.js
+++ src/block/plugin/sticky/.document-ready.js
@@ -1,6 +1,8 @@
 if($(document).width() < 575) {
 	$("#img").sticky({
 		topSpacing:0,
+		bottomSpacing:500,
 		className: 'is--sticky'
 	});
+	$('#img').on('sticky-bottom-unreached', function() { console.log("Bottom unreached"); });
 } 
